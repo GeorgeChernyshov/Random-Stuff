a13b7ad3197830ade3d332a2a60ec7c68dd76b50
GeorgeChernyshov <ridiculousman@yandex.ru>
2016-10-05 13:27:06 +0400

    Update homework190916.py

--- homework190916.py
+++ homework190916.py
@@ -18,14 +18,14 @@ for i in range(0,n):
     str = input()
     bk[i] = str.split()
 r = np.dot(a,b)
-a11 = a[:p/2, :p/2]
-a12 = a[:p/2, p/2:p]
-a21 = a[p/2:p, :p/2]
-a22 = a[p/2:p, p/2:p]
-b11 = b[:p/2, :p/2]
-b12 = b[:p/2, p/2:p]
-b21 = b[p/2:p, :p/2]
-b22 = b[p/2:p, p/2:p]
+a11 = a[:p//2, :p//2]
+a12 = a[:p//2, p//2:p]
+a21 = a[p//2:p, :p//2]
+a22 = a[p//2:p, p//2:p]
+b11 = b[:p//2, :p//2]
+b12 = b[:p//2, p//2:p]
+b21 = b[p//2:p, :p//2]
+b22 = b[p//2:p, p//2:p]
 c = np.zeros((p,p))
 p1 = np.dot((a11+a22),(b11+b22))
 p2 = np.dot((a21+a22), b11)
@@ -34,9 +34,11 @@ p4 = np.dot(a22,(b21-b11))
 p5 = np.dot((a11+a12),b22)
 p6 = np.dot((a21-a11),(b11+b12))
 p7 = np.dot((a12-a22),(b21+b22))
-c[:p/2, :p/2] = p1+p4-p5+p7
-c[:p/2, p/2:p] = p3+p5
-c[p/2:p, :p/2] = p2+p4
-c[p/2:p, p/2:p] = p1-p2+p3+p6
-ans = c[:n, :n]
-print(ans)
+c[:p//2, :p//2] = p1+p4-p5+p7
+c[:p//2, p//2:p] = p3+p5
+c[p//2:p, :p//2] = p2+p4
+c[p//2:p, p//2:p] = p1-p2+p3+p6
+for i in range(0,n):
+    for j in range(0,n):
+        print(int(c[i][j]), end = " "),
+    print( )
