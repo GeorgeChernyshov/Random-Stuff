411e7aaa77a73528c3be3ea5bee95caad9d49343
GeorgeChernyshov <ridiculousman@yandex.ru>
2016-10-06 00:25:27 +0400

    Update homework190916.py

--- homework190916.py
+++ homework190916.py
@@ -5,16 +5,11 @@ def resize(p):
         p = p*2
     return p
 
-def read(n,arr,p):
+def read(n,arr):
     ak = arr[:n, :n]
     for i in range(0,n):
         str1 = input()
         ak[i] = str1.split()
-    return arr
-
-def create(n,arr,p):
-    arr = np.empty((p,p))
-    read(n,arr,p)
     return arr
 
 def strassen(a,b,p,c):
@@ -41,18 +36,12 @@ def strassen(a,b,p,c):
         p5 = strassen((a11+a12),b22, p//2, p5)
         p6 = strassen((a21-a11),(b11+b12), p//2, p6)
         p7 = strassen((a12-a22),(b21+b22), p//2, p7)
+        c[:p//2, :p//2] = p1+p4-p5+p7
+        c[:p//2, p//2:p] = p3+p5
+        c[p//2:p, :p//2] = p2+p4
+        c[p//2:p, p//2:p] = p1-p2+p3+p6
     else:
-        p1 = np.dot((a11+a22),(b11+b22))
-        p2 = np.dot((a21+a22), b11)
-        p3 = np.dot(a11,(b12-b22))
-        p4 = np.dot(a22,(b21-b11))
-        p5 = np.dot((a11+a12),b22)
-        p6 = np.dot((a21-a11),(b11+b12))
-        p7 = np.dot((a12-a22),(b21+b22))
-    c[:p//2, :p//2] = p1+p4-p5+p7
-    c[:p//2, p//2:p] = p3+p5
-    c[p//2:p, :p//2] = p2+p4
-    c[p//2:p, p//2:p] = p1-p2+p3+p6
+        c[:p, :p] = np.dot(a,b)    
     return c
     
 
@@ -64,13 +53,13 @@ def ansout(c,n):
 
 n = input()
 n = int(n)
-a = []
-b = []
 p = 1
-p = resize(p)
-a = create(n,a,p)
-b = create(n,b,p)
-c = np.empty((p,p), dtype = a.dtype)
-с = strassen(a,b,p,c)
+u = resize(p)
+a = np.empty((u,u))
+b = np.empty((u,u))
+read(n,a)
+read(n,b)
+c = np.empty((u,u), dtype = a.dtype)
+с = strassen(a,b,u,c)
 c.dtype = a.dtype
 ansout(c,n)
