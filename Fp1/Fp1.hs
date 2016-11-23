40b8a4e496fff3932ea0508d12e5aa844de04e5e
GeorgeChernyshov <ridiculousman@yandex.ru>
2016-11-23 12:47:10 +0300

    Add files via upload

---
+++ Fp1/Fp1.hs
@@ -0,0 +1,38 @@
+head' :: [a] -> a
+head' (xs:_) = xs
+
+tail' :: [a] -> [a]
+tail' [] = []
+tail' (_:xs) = xs
+
+take' :: Int -> [a] -> [a]
+take' 0 x = []
+take' a (x:xs) = (x:take' (a-1) xs)
+                         
+
+drop' :: Int -> [a] -> [a]
+drop' 0 x = x
+drop' a [] = []
+drop' a (x:xs) = (drop' (a-1) xs)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f [] = []
+filter' f (x:xs) = if f x == True
+                      then (x:filter' f xs)
+                      else (filter' f xs)  
+
+foldl' :: (a -> b -> a) -> a -> [b] -> a
+foldl' f z [] = z
+foldl' f z (x:xs) = foldl' f (f z x) xs
+
+concat' :: [a] -> [a] -> [a]
+concat' x [] = x
+concat' [] (y:ys) = (y:concat' [] ys)
+concat' (x:xs) y = (x:concat' xs y)
+
+quickSort' :: Ord a => [a] -> [a]
+quickSort' [] = []
+quickSort' (x:xs) = 
+    let sSort = quickSort'(filter' (<=x) xs) 
+        rSort = quickSort'(filter' (>x) xs)
+    in concat' sSort (concat' [x]  rSort)
