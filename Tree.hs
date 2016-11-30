c6d66bc7f28cc8cb2c12566b623436b8a252f6b6
GeorgeChernyshov <ridiculousman@yandex.ru>
2016-11-30 16:21:20 +0300

    Add files via upload

---
+++ Tree.hs
@@ -0,0 +1,24 @@
+import Prelude hiding (lookup)
+
+data BinaryTree k v = Node k v (BinaryTree k v)(BinaryTree k v) | Nil deriving Show
+
+lookup :: Ord k => k -> BinaryTree k v -> Maybe v
+lookup _ Nil = Nothing;
+lookup k2 (Node k v lt rt) | k == k2 = Just v 
+                           | otherwise = if k2 > k then lookup k2 rt else lookup k2 lt
+
+insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
+insert k1 v1 Nil = (Node k1 v1 Nil Nil)
+insert k1 v1 (Node k v lt rt) | k1 == k = (Node k v1 lt rt)
+                              | otherwise = if k1 > k then (Node k v lt (insert k1 v1 rt)) else (Node k v (insert k1 v1 lt) rt)
+
+merge :: Ord k => BinaryTree k v -> BinaryTree k v -> BinaryTree k v
+merge x Nil = x
+merge Nil x = x
+merge (Node k v lt rt) (Node k1 v1 lt1 rt1) | k1 < k = (Node k v (merge (Node k1 v1 lt1 rt1) lt) rt)
+                                            | otherwise = (Node k v lt (merge (Node k1 v1 lt1 rt1) rt))
+
+delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
+delete k1 Nil = Nil
+delete k1 (Node k v lt rt) | k1 == k = merge lt rt
+                           | otherwise = if k1 > k then (Node k v lt (delete k1 rt)) else (Node k v (delete k1 lt) rt) 
