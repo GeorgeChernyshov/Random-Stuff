2d8d1f37e708f417fbd26b046c802b697ce871b8
GeorgeChernyshov <ridiculousman@yandex.ru>
2016-12-07 23:00:50 +0400

    Update 7.sql

--- Sqlite/7.sql
+++ Sqlite/7.sql
@@ -1,4 +1,5 @@
 select Country.Name
 from Country inner join City on City.CountryCode = Country.Code
 group by Country.Code
-having sum(City.Population) <= 0.5*(Country.Population) and Country.Population > 0;
+having sum(City.Population) < 0.5*(Country.Population) and Country.Population > 0
+order by Country.Name;
