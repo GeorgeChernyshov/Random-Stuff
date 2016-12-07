6d75e2906be62b1abd926bdd1841ba6c054cbdb7
GeorgeChernyshov <ridiculousman@yandex.ru>
2016-12-07 20:37:24 +0400

    Update 4.sql

--- Sqlite/4.sql
+++ Sqlite/4.sql
@@ -1,5 +1,4 @@
 select Country.Name, count(City.Population)
-from Country inner join City on Country.Code = City.CountryCode
-where City.Population >= 1000000
+from Country left join City on Country.Code = City.CountryCode and City.Population >= 1000000
 group by Country.Name
 order by count(City.Population) desc;
