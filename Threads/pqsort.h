eed159ae0cf60a0939ceb48ab4431dec2281bb29
GeorgeChernyshov <ridiculousman@yandex.ru>
2017-02-03 00:08:06 +0400

    Delete pqsort.h

--- Threads/pqsort.h
+++
@@ -1,8 +0,0 @@
-#ifndef PQSORT_H_INCLUDED
-#define PQSORT_H_INCLUDED
-#include "thread_pool.h"
-
-void sort_array(int depth, int max_depth, int* x, int N, struct ThreadPool* pool, int threads_num);
-int comp (const int *i, const int *j);
-
-#endif // PQSORT_H_INCLUDED
