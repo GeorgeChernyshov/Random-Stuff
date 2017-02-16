7e3e6f434acee67fd8e0aadc3969b4665d0037fb
GeorgeChernyshov <ridiculousman@yandex.ru>
2017-02-17 00:05:26 +0400

    Add files via upload

---
+++ Threads/src/pqsort.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <pthread.h>
+#include "thread_pool.h"
+
+void sort_array(int depth, int max_depth, int* x, int N, struct ThreadPool* pool, int threads_num);
+int comp (const int *i, const int *j);
+struct Task* create_task(void);
+void destroy_task(struct Task* task);
+
---
+++ Threads/src/thread_pool.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "wsqueue.h"
+
+struct Task {
+    struct list_node link;
+	void (*f)(void *);
+	void* arg;
+	pthread_cond_t cond;
+	pthread_mutex_t mutex;
+	int complete;
+};
+
+struct ThreadPool {
+    pthread_t* threads;
+    struct wsqueue* tasks;
+    unsigned num;
+};
+
+void thpool_init(struct ThreadPool* pool, unsigned threads_nm);
+void thpool_submit(struct ThreadPool* pool, struct Task* task);
+void thpool_wait(struct Task* task);
+void thpool_finit(struct ThreadPool* pool);
+
