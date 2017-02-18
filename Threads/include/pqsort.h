ad87c4e5a702882ac451e71cbb96552ba50ab702
GeorgeChernyshov <ridiculousman@yandex.ru>
2017-02-18 20:52:22 +0400

    Add files via upload

--- Threads/include/pqsort.h
+++ Threads/include/pqsort.h
@@ -4,6 +4,5 @@
 
 void sort_array(int depth, int max_depth, int* x, int N, struct ThreadPool* pool);
 int comp (const int *i, const int *j);
-struct Task* create_task(void);
-void destroy_task(struct Task* task);
+struct Task* create_task(struct Task* task);
 
