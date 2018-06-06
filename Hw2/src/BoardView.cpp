cf9ec1aab5580fdcec4f94349ac42a42e91dcf52
GeorgeChernyshov <ridiculousman@yandex.ru>
2018-06-07 00:42:58 +0300

    Update BoardView.cpp

--- Hw2/src/BoardView.cpp
+++ Hw2/src/BoardView.cpp
@@ -1,10 +1,10 @@
 #include "BoardView.h"
 #include <cstdio>
 #include <cstdlib>
-
+//Why is this in cpp file?
 View::View(Board &b, bool show) : board_(b), show_(show){
 }
-
+//Nuff said
 void View::showBoard(){
     for(int i = 0; i < 10; i++){    
         for(int j = 0; j < 10; j++){       
@@ -26,11 +26,12 @@ void View::showBoard(){
     
     printf("\n");
 }
-
+//The most interesting stuff
 void View::doGameCycle(){
     int x, y;
     char playerID = 0;
     bool done;
+    //This is definitely not GameCycle, more like the whole game
     while(board_.isWin() == play){
         done = false;
         if(show_){
@@ -61,6 +62,7 @@ void View::doGameCycle(){
         }
         
         state status = board_.isWin();
+        //This is... not optimal in terms of code length
         if(status == x_wins){
             if(show_){
                 showBoard();
@@ -81,7 +83,7 @@ void View::doGameCycle(){
             }
             printf("Draw!\n");
         }
-        
+        //^= 1
         playerID = 1 - playerID;
     }
 }
