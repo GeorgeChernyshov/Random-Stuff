c7ae098edfa7c889ab12a6291d8a8aa6e1377dd1
GeorgeChernyshov <ridiculousman@yandex.ru>
2018-06-09 00:03:52 +0300

    Update Store3.sql

--- Sqlite/Store3.sql
+++ Sqlite/Store3.sql
@@ -1,15 +1,10 @@
 select good.name from good
-	inner join category_has_good 
-		on good.id = good_id
-	inner join category 
-		on category.id = category_id
+	inner join category_has_good on good.id = good_id
+	inner join category on category.id = category_id
 	where category.name = "Cakes"
 union
 select good.name from good
-	inner join sale_has_good 
-		on good.id = good_id
-	inner join sale
-		on sale.id = sale_id
-	inner join status
-		on status.id = sale.status_id
+	inner join sale_has_good on good.id = good_id
+	inner join saleon sale.id = sale_id
+	inner join status on status.id = sale.status_id
 	where status.name = "delivering";
